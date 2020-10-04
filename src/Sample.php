3a26d566739f8d36105c140eb578308e0414ff59
inoue441 <y.inoue@jfd.co.jp>
2020-10-04 20:25:15 +0900

    文字Lを渡したとき、文字列Noを返す 😇[Green]
    ./vendor/bin/phpunit --color tests/SampleTest.php
    PHPUnit 9.4.0 by Sebastian Bergmann and contributors.
    
    ..                                                                  2 / 2 (100%)
    
    Time: 00:00.003, Memory: 4.00 MB
    
    OK (2 tests, 2 assertions)

--- src/Sample.php
+++ src/Sample.php
@@ -2,8 +2,11 @@
 
 class Sample
 {
-    public function solve()
+    public function solve(string $input): string
     {
+        if ($input === 'L') {
+            return 'No';
+        }
         return 'Yes';
     }
 }
