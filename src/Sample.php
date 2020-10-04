d7cb990d41e64c944fc7738178624a9ef8fb7fc1
inoue441 <y.inoue@jfd.co.jp>
2020-10-04 20:49:45 +0900

    文字列UULを渡したとき、文字列Noを返す 😇[Green]
    ./vendor/bin/phpunit --color tests/SampleTest.php
    PHPUnit 9.4.0 by Sebastian Bergmann and contributors.
    
    ...                                                                 3 / 3 (100%)
    
    Time: 00:00.014, Memory: 4.00 MB
    
    OK (3 tests, 3 assertions)

--- src/Sample.php
+++ src/Sample.php
@@ -2,11 +2,31 @@
 
 class Sample
 {
+    const YES = 'Yes';
+    const NO = 'No';
+
+    /**
+     * 奇数文字目が「R, U, D」のいずれか、かつ、偶数文字目が「L, U, D」のいずれか => Yes
+     * 奇数文字目が「L」、または、偶数文字目が「R」                               => No
+     *
+     * @param  string  $input
+     * @return string
+     */
     public function solve(string $input): string
     {
-        if ($input === 'L') {
-            return 'No';
+        for ($i = 0, $l = strlen($input); $i < $l; $i++) {
+            $seq = $i + 1;
+
+            // 奇数文字目が「R, U, D」のいずれか"ではない" => No
+            if ($seq % 2 !== 0 && !in_array($input[$i], ['R', 'U', 'D'])) {
+                return self::NO;
+            }
+            // 偶数文字目が「L, U, D」のいずれか"ではない" => No
+            if ($seq % 2 === 0 && !in_array($input[$i], ['L', 'U', 'D'])) {
+                return self::NO;
+            }
         }
-        return 'Yes';
+
+        return self::YES;
     }
 }
